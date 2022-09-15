7ff39f90a6a0e2277bd7fb714deef489fa692de1
laukhin-alex <alex.laukhin@yandex.ru>
2022-09-15 12:08:26 +0300

    Initial Commit

---
+++ MultiApp/MultiAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultiAppApp.swift
+//  MultiApp
+//
+//  Created by Александр on 15.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct MultiAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
