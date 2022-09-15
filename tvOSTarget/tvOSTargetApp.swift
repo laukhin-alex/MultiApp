b2eeb271b1034b2f14706e32b17eb5d8dcb72d95
laukhin-alex <alex.laukhin@yandex.ru>
2022-09-15 13:07:56 +0300

    tvOSTarget was added

---
+++ tvOSTarget/tvOSTargetApp.swift
@@ -0,0 +1,17 @@
+//
+//  tvOSTargetApp.swift
+//  tvOSTarget
+//
+//  Created by Александр on 15.09.2022.
+//
+
+import SwiftUI
+
+
+struct tvOSTargetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
