4f59951a6a5c8a91a8fcdc6ba7805951be441cdb
laukhin-alex <alex.laukhin@yandex.ru>
2022-09-15 12:39:07 +0300

    Ios staged

---
+++ iOSTarget/iOSTargetApp.swift
@@ -0,0 +1,16 @@
+//
+//  iOSTargetApp.swift
+//  iOSTarget
+//
+//  Created by Александр on 15.09.2022.
+//
+
+import SwiftUI
+
+struct iOSTargetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
